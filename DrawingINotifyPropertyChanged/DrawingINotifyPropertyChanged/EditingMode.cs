4607df4a6e60289403f9df59ae7106b9d3084e0b
Łukasz Paczyński <luki00765@gmail.com>
2014-12-27 12:51:55 +0100

    drawing

---
+++ DrawingINotifyPropertyChanged/DrawingINotifyPropertyChanged/EditingMode.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace DrawingINotifyPropertyChanged
+{
+	public class EditingMode : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+		private InkCanvasEditingMode _editMode;
+		public InkCanvasEditingMode EditMode
+		{
+			get { return _editMode; }
+			set
+			{
+				_editMode = value;
+				if (PropertyChanged != null)
+					PropertyChanged(this, new PropertyChangedEventArgs("EditMode"));
+			}
+		}
+
+		private string _text;
+
+		public string Text
+		{
+			get { return _text; }
+			set
+			{
+				_text = value;
+				if (PropertyChanged != null)
+					PropertyChanged(this, new PropertyChangedEventArgs("Text"));
+			}
+		}
+
+		public EditingMode()
+		{
+			EditMode = InkCanvasEditingMode.Ink;
+			Text = "Ink";
+		}
+	}
+}
---
+++ DrawingINotifyPropertyChanged/DrawingINotifyPropertyChanged/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DrawingINotifyPropertyChanged
+{
+	/// <summary>
+	/// Logika interakcji dla klasy MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		EditingMode editingMode = new EditingMode();
+		public MainWindow()
+		{
+			InitializeComponent();
+			this.DataContext = editingMode;
+		}
+
+		private void InkBtn_Click(object sender, RoutedEventArgs e)
+		{
+			editingMode.EditMode = InkCanvasEditingMode.Ink;
+			editingMode.Text = "Ink";
+		}
+
+		private void DrawBtn_Click(object sender, RoutedEventArgs e)
+		{
+			editingMode.EditMode = InkCanvasEditingMode.Select;
+			editingMode.Text = "Draw";
+		}
+	}
+}
